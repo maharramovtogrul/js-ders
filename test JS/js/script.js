ae15ebdb79d68cdc08dc9d4893eabe63e07f6629
maharramovtogrul <meherremovtogrul@gmail.com>
2022-05-26 19:04:49 +0400

    js

---
+++ test JS/js/script.js
@@ -0,0 +1,62 @@
+
+
+// 15-e bolunen
+
+// var sum=0
+// for (let i = 15; i <= 100; i++) {
+//     let total=0
+//     if (i%15==0) {
+      
+//       console.log(i +" 15 bolunenler");
+//     }
+
+//   }
+
+// // 3-e bolunen qaliqla;
+
+//   var sum=0
+// for (let i = 0; i <= 100; i++) {
+//     let total=0
+//     if (i%3) {
+      
+//       console.log(i);
+//     }
+
+//   }
+
+// // 5-e bolunen qaliqla;
+
+//   var sum=0
+// for (let i = 0; i <= 100; i++) {
+//     let total=0
+//     if (i%5) {
+      
+//       console.log(i);
+//     }
+
+//   }
+
+
+// // 3-e bolunen qaliqsiz
+
+//   var sum=0
+// for (let i = 0; i <= 100; i++) {
+//     let total=0
+//     if (i%3 == 0) {
+      
+//       console.log(i);
+//     }
+
+//   }
+
+// // 5-e bolunen qaliqsiz
+
+//   var sum=0
+// for (let i = 0; i <= 100; i++) {
+//     let total=0
+//     if (i%5 == 0) {
+      
+//       console.log(i);
+//     }
+
+//   }
